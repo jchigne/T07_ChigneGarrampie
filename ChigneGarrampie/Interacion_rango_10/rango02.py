acfe9c6a8ced3ab8064a5103d5e222ac00231c9a
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:53:15 -0500

    rango02

---
+++ ChigneGarrampie/Interacion_rango_10/rango02.py
@@ -0,0 +1,10 @@
+#genera una lista del 0 al a
+import os
+a=int(os.sys.argv[1])
+
+print("la lista es:")
+for i in range(a):
+    print(i)
+print("La lista del 0 al",a,"es")
+
+#fin_iterador de rango
