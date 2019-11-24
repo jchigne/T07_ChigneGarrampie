36f96c2b848924b478ec06c95f163079371ca45a
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:53:24 -0500

    rango03

---
+++ ChigneGarrampie/Interacion_rango_10/rango03.py
@@ -0,0 +1,10 @@
+#listado del nuemro a hasta el b
+import os
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+print("la lista es:")
+for j in range(a,b+1):
+    print(j)
+print("lista desde ",a,"hasta: ",b)
+
+#fin_iterador de rango
