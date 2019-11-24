2e9b7c1a554e66bffdb7c74e538da816283cc667
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:54:06 -0500

    rango08

---
+++ ChigneGarrampie/Interacion_rango_10/rango08.py
@@ -0,0 +1,10 @@
+#mostrar el numero de goles
+import os
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+print("contando:")
+for goles in range(a,b):
+    print("goles"+str(goles))
+#fin_iterador de rango
+print("Fin de la cuenta regresiba")
+
