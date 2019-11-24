5ede0e105330f2e9e8c0d305fd0b60a2759f65c3
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:53:58 -0500

    rango07

---
+++ ChigneGarrampie/Interacion_rango_10/rango07.py
@@ -0,0 +1,9 @@
+#muestra la palabra Flamenco Campeon repitiendose.
+import os
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+print("Se repite muchas veses:")
+for river_vs_flamenco in range(a,b,2):
+    print("Flamenco Campeon")
+#fin_iterador de rango
+print("Dos goles de Gabigol")
