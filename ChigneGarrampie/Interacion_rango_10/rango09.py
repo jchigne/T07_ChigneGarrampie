a447c3e7b84833f53e88d88d93bb1035934d89a3
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:54:16 -0500

    rango09

---
+++ ChigneGarrampie/Interacion_rango_10/rango09.py
@@ -0,0 +1,9 @@
+#mostrar el numero de goles
+import os
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+print("contando:")
+for filtros in range(a,b,2):
+    print("filtros"+str(filtros))
+#fin_iterador de rango
+print("Fin de los filtos")
