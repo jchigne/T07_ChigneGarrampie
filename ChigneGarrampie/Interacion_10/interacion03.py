3cb5c291bfbbd3911a758ad86e80c57982c3211f
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:56:02 -0500

    interacion03

---
+++ ChigneGarrampie/Interacion_10/interacion03.py
@@ -0,0 +1,20 @@
+#declaracion de datos
+import os
+MSG=""
+
+#imput
+MSG=os.sys.argv[1].upper()
+
+#el mensaje es
+print("El mensaje encriptado es:")
+#bucle
+for letra in MSG:
+    if letra == "A":
+        print("quiero ser siempre")
+    if letra == "B":
+        print("el mejor,mejor que nadie mas ")
+    if letra == "C":
+        print("atraparlos mi meta es")
+
+#fin_interacion
+print("Fin del bucle")
