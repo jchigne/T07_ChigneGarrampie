4542be091e86e9b640d89dd38ab7bbb1fc4469c6
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:57:01 -0500

    interacion09

---
+++ ChigneGarrampie/Interacion_10/interacion09.py
@@ -0,0 +1,20 @@
+#declaracion de datos
+import os
+MSG=""
+
+#imput
+MSG=os.sys.argv[1].upper()
+
+#el mensaje es
+print("El mensaje encriptado es:")
+#bucle
+for letra in MSG:
+    if letra == "A":
+        print("El jurado nacional de elecciones")
+    if letra == "B":
+        print("informo, que de las 22 lsitas inscritas")
+    if letra == "C":
+        print("solo 11 han sido admitidas")
+
+#fin_interacion
+print("Fin del bucle")
