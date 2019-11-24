838e59b00202ba4a7f5f22bc9d0fd23859bbea39
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:57:10 -0500

    interacion10

---
+++ ChigneGarrampie/Interacion_10/interacion10.py
@@ -0,0 +1,20 @@
+#declaracion de datos
+import os
+MSG=""
+
+#imput
+MSG=os.sys.argv[1].upper()
+
+#el mensaje es
+print("El mensaje encriptado es:")
+#bucle
+for letra in MSG:
+    if letra == "A":
+        print("Heroe Nacional")
+    if letra == "B":
+        print("Jose Domingo Perez")
+    if letra == "C":
+        print("Fiscla del pueblo")
+
+#fin_interacion
+print("Fin del bucle")
