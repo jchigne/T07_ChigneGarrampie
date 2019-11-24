65485d8cc0bfa59446351901f85ee19a2ad4f7b3
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:53:41 -0500

    rango05

---
+++ ChigneGarrampie/Interacion_rango_10/rango05.py
@@ -0,0 +1,9 @@
+#mostrar el numero de vuelto
+import os
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+print("contando:")
+for contando in range(a,b):
+    print("contando"+str(contando))
+#fin_iterador de rango
+print("Fin de la cuenta regresiba")
