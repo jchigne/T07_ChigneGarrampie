4e450575f494e6597a872149008d91a013a43c0c
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:56:15 -0500

    interacion04

---
+++ ChigneGarrampie/Interacion_10/interacion04.py
@@ -0,0 +1,20 @@
+#declaracion de datos
+import os
+MSG=""
+
+#imput
+MSG=os.sys.argv[1].upper()
+
+#el mensaje es
+print("El mensaje encriptado es:")
+#bucle
+for letra in MSG:
+    if letra == "A":
+        print("Somos libres")
+    if letra == "B":
+        print("seamoslo siempre")
+    if letra == "C":
+        print("Viva el Peru")
+
+#fin_interacion
+print("Fin del bucle")
