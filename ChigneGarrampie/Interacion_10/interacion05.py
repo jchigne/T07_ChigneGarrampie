42c5fc4398cd3a18566c638536248ab7138f8ec5
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:56:24 -0500

    interacion05

---
+++ ChigneGarrampie/Interacion_10/interacion05.py
@@ -0,0 +1,20 @@
+#declaracion de datos
+import os
+MSG=""
+
+#imput
+MSG=os.sys.argv[1].upper()
+
+#el mensaje es
+print("El mensaje encriptado es:")
+#bucle
+for letra in MSG:
+    if letra == "A":
+        print("Para mayor informacion")
+    if letra == "B":
+        print("visite nuestra pagina web")
+    if letra == "C":
+        print("WWW.acerosarequipa.com.pe")
+
+#fin_interacion
+print("Fin del bucle")
