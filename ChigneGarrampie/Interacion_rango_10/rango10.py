88a634e202cd98630578ad04386c337f12eca69c
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:54:25 -0500

    rango10

---
+++ ChigneGarrampie/Interacion_rango_10/rango10.py
@@ -0,0 +1,9 @@
+#mostrar el numero de goles
+import os
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+print("contando:")
+for mas_3 in range(a,b,3):
+    print("+3"+str(mas_3))
+#fin_iterador de rango
+print("Fin de las sumatoria")
