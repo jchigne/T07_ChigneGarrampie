3935e0e7c2ff23daa9c80a77b2d827bc1b803c4e
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:53:50 -0500

    rango06

---
+++ ChigneGarrampie/Interacion_rango_10/rango06.py
@@ -0,0 +1,9 @@
+#muestra la palabra River vs Flamenco repitiendose.
+import os
+a=int(os.sys.argv[1])
+b=int(os.sys.argv[2])
+print("Se repite muchas veses:")
+for river_vs_flamenco in range(a,b,4):
+    print("River vs flamenco")
+#fin_iterador de rango
+print("La copa libertadores es mi obseccion")
