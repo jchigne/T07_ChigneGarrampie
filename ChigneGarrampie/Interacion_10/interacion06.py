6fe9798a53c82ce45debbc3e9b29cb4f3cefc74a
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:56:32 -0500

    interacion06

---
+++ ChigneGarrampie/Interacion_10/interacion06.py
@@ -0,0 +1,20 @@
+#declaracion de datos
+import os
+MSG=""
+
+#imput
+MSG=os.sys.argv[1].upper()
+
+#el mensaje es
+print("El mensaje encriptado es:")
+#bucle
+for letra in MSG:
+    if letra == "A":
+        print("La cuarta maravilla")
+    if letra == "B":
+        print("del mundo es para")
+    if letra == "C":
+        print("Machu picchu")
+
+#fin_interacion
+print("Fin del bucle")
