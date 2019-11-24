aa207d886ab3885f7c22d9deb65d6dcd4e2d49ff
Chigne Garrampie <jchigne@unprg.edu.pe>
2019-11-23 21:56:40 -0500

    interacion07

---
+++ ChigneGarrampie/Interacion_10/interacion07.py
@@ -0,0 +1,20 @@
+#declaracion de datos
+import os
+MSG=""
+
+#imput
+MSG=os.sys.argv[1].upper()
+
+#el mensaje es
+print("El mensaje encriptado es:")
+#bucle
+for letra in MSG:
+    if letra == "A":
+        print("Nosotros bamos")
+    if letra == "B":
+        print("A ganar en lima ")
+    if letra == "C":
+        print("En la final de la libertadores")
+
+#fin_interacion
+print("Fin del bucle")
